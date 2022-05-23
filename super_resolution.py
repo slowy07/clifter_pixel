ad54a9d9275334e74b6b0ecf548b684731f64ab0
slowy07 <slowy.arfy@gmail.com>
2022-05-24 03:35:19 +0700

    update fetch
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- super_resolution.py
+++ super_resolution.py
@@ -12,6 +12,7 @@ superresolution_checkpoint_table = {
     "RealESRGAN_x4plus": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth",
 }
 
+
 class ReplaceGrad(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x_forward, x_backward):
@@ -21,5 +22,6 @@ class ReplaceGrad(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_in):
         return None, grad_in.sum_to_size(ctx.shape)
+
 
 replace_grad = ReplaceGrad.apply
